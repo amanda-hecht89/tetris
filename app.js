9bb08f3d7e14fca99c659d0ceddd960b8fd580a5
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-09 09:56:18 -0600

    build shapes in js

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const width = 10;
+    const height = 200;
+
+    const grid = document.querySelector('.grid');
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+
+    // build tetrominoes
+
+    const lTetris = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width * 2 + 1, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const zTetris = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ];
+
+    const tTetris = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ];
+
+    const oTetris = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+    ];
+
+    const iTetris = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+    ];
+
+    const theTetris = [lTetris, iTetris, oTetris, tTetris, zTetris];
+
+
+
 
 
 
